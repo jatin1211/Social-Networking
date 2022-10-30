73656ed977c843a0762de787748289b5af389f63
Jatin_Saini <sainijatin247@gmail.com>
2022-10-30 16:11:34 -0400

    user routes working

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require("../../controllers/thought-controller");
+
+// /api/comments/<pizzaId>
+router.route("/").get(getAllThoughts).post(createThought);
+router.route("/:id").get(getThoughtById).put(updateThought).delete(deleteThought);
+
+
+router.route('/:thoughtId/reactions')
+.post(addReaction)
+.delete(removeReaction)
+
+
+module.exports = router;
